feda7dfcd20f8ded31459c4887bd9263deaf0df8
Michael Serajnik <ms@mserajnik.at>
2018-11-05 17:33:35 +0100

    Change migration runner to no longer require node-sqlite

--- storage/migrate.js
+++ storage/migrate.js
@@ -1,7 +1,8 @@
 const path = require('path')
+
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
-const migrations = require('./migrations/runner')
 const config = require('../server/config/app-db')
+const migrations = require('./migrations/runner')
 
-migrations.up(config.dbPath)
+migrations.run(config.dbPath)
