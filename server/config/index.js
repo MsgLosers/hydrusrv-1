733e1a568fe328115b2836bc44c4fdc5c9eaaac4
Michael Serajnik <ms@mserajnik.at>
2018-12-09 01:54:34 +0100

    docs: Update changelog and version numbers for release 5.0.0

--- server/config/index.js
+++ server/config/index.js
@@ -13,7 +13,7 @@ if (contentDbPath.startsWith('.')) {
 }
 
 module.exports = {
-  version: '4.4.0',
+  version: '5.0.0',
   url: process.env.URL,
   port: process.env.PORT || 8000,
   apiBase: process.env.API_BASE || '/api',
